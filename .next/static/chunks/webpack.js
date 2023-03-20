ba92badb5e3fc5d5cf53db94d0fc3b93ff2157f2
Rahul Dhawan <32161208+RAhuLDHawaN-cmd@users.noreply.github.com>
2023-03-20 14:59:01 -0400

    adjust nav

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0e600b6e6be894c0"; }
+/******/ 		__webpack_require__.h = function() { return "47dae2d000d893ec"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
