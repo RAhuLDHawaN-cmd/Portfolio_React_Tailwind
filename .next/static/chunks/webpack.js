934e2269404313597720759155782d46124601e5
Rahul Dhawan <32161208+RAhuLDHawaN-cmd@users.noreply.github.com>
2023-03-23 09:52:55 -0400

    add

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6da949b24082be4f"; }
+/******/ 		__webpack_require__.h = function() { return "14458ed404d080d0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -39,7 +39,7 @@ export default function Home() {
       <main className="bg-white px-10 dark:bg-zinc-900 md:px-20 lg:px-40">
         <section className="min-h-screen" id="banner">
           <nav className=" bg-white pt-10 mb-12 flex justify-evenly sticky top-0 z-50 dark:bg-zinc-900 dark:text-white ">
-            <h1 className="font-burtons text-xl ml-20">
+            <h1 className="font-burtons text-xl ml-12">
             <a href="/">MatchStick</a>
             </h1>
          
