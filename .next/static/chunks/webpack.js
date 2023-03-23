96b5b3231ac4e413a4a908e31ea5f81931eca0a4
Rahul Dhawan <32161208+RAhuLDHawaN-cmd@users.noreply.github.com>
2023-03-23 09:11:10 -0400

    added content change

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "fca19e59593d9b10"; }
+/******/ 		__webpack_require__.h = function() { return "fe0874af2cfb2f44"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -116,7 +116,7 @@ export default function Home() {
         </section>
         <section id="services">
           <div>
-            <h3 className="text-3xl py-1 dark:text-white heads">Services I offer</h3>
+            <h3 className="text-3xl py-1 dark:text-white heads">Services offered</h3>
             <p className="text-md py-5 leading-8 text-gray-800 dark:text-gray-200 md:text-xl desc">
               Since the beginning of my journey as a freelance designer and
               developer, I've done remote work for
